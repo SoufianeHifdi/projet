4aeab76c9afe90a9815f4f73461da7670f5d265b
SoufianeHifdi <41968685+SoufianeHifdi@users.noreply.github.com>
2022-05-24 11:42:46 +0200

    Update executor.sh

--- executor.sh
+++ executor.sh
@@ -4,16 +4,19 @@
 source /home/sfifina/miniconda3/etc/profile.d/conda.sh
 
 
-# cd yolov4-deepsort
-# conda activate yolov4-cpu
-# python3 object_tracker.py --video ./videos/video_cam1.mp4 --model yolov4
-# python3 object_tracker.py --video ./videos/video_cam2.mp4 --model yolov4
-# python3 object_tracker.py --video ./videos/video_cam3.mp4 --model yolov4
-# python3 object_tracker.py --video ./videos/video_cam4.mp4 --model yolov4
+# Execution du tracking
+cd yolov4-deepsort
+conda activate yolov4-cpu
+python3 object_tracker.py --video ./videos/video_cam1.mp4 --model yolov4
+python3 object_tracker.py --video ./videos/video_cam2.mp4 --model yolov4
+python3 object_tracker.py --video ./videos/video_cam3.mp4 --model yolov4
+python3 object_tracker.py --video ./videos/video_cam4.mp4 --model yolov4
 
+# Pour utiliser tinyYolo : 
 # python3 object_tracker.py --weights ./checkpoints/yolov4-tiny-416 --model yolov4 --video ./videos/video_cam3.mp4 --tiny 
 # python3 object_tracker.py --weights ./checkpoints/yolov4-tiny 416 --model yolov4 --video ./data/video/test.mp4 --tiny 
 
+#Partie Execution du Reid
 cd ~/Pres/deep-person-reid
 conda activate torchreid 
 python3 object_reid.py
