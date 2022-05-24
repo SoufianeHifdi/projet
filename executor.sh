e9ee84ae18e921bfc95f2a468ccfb91412005309
SoufianeHifdi <41968685+SoufianeHifdi@users.noreply.github.com>
2022-05-24 11:41:16 +0200

    Add files via upload

---
+++ executor.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Since conda is a bash function and bash functions can not be propagated to independent shells (e.g. opened by executing a bash script), one has to add the line
+source /home/sfifina/miniconda3/etc/profile.d/conda.sh
+
+
+# cd yolov4-deepsort
+# conda activate yolov4-cpu
+# python3 object_tracker.py --video ./videos/video_cam1.mp4 --model yolov4
+# python3 object_tracker.py --video ./videos/video_cam2.mp4 --model yolov4
+# python3 object_tracker.py --video ./videos/video_cam3.mp4 --model yolov4
+# python3 object_tracker.py --video ./videos/video_cam4.mp4 --model yolov4
+
+# python3 object_tracker.py --weights ./checkpoints/yolov4-tiny-416 --model yolov4 --video ./videos/video_cam3.mp4 --tiny 
+# python3 object_tracker.py --weights ./checkpoints/yolov4-tiny 416 --model yolov4 --video ./data/video/test.mp4 --tiny 
+
+cd ~/Pres/deep-person-reid
+conda activate torchreid 
+python3 object_reid.py
+
+exit 0
